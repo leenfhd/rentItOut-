93aa5583465795011184222af104d4993be7f7ac
RaghadMM <s12027617@stu.najah.edu>
2024-11-02 23:36:14 +0200

    update rental router

--- routes/rentalRouter.js
+++ routes/rentalRouter.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const rentalController = require("../controllers/rentalController"); // Adjust the path as necessary
 
-// Register a new rental
-router.post("/Rentals", rentalController.registerRental);
-router.get("/AllRentals", rentalController.getAllRentals);
-router.get("/Rentals", rentalController.getRentals);
-router.get("/LateRentals", rentalController.getLateRentals);
-router.post("/Fees", rentalController.updateLateFeesController);
-router.delete("/:id", rentalController.deleteRental);
-router.put("/:id", rentalController.updateRental);
-router.post('/decision', rentalController.acceptOrDenyRental);
+
+router.post("/", rentalController.registerRental);//register a new rental
+router.get("/AllRentals", rentalController.getAllRentals);//get all rentals for admin
+router.get("/", rentalController.getRentals);// search on items by specific field
+router.get("/LateRentals", rentalController.getLateRentals);//get late unretrieved rentals
+router.put("/Fees", rentalController.updateLateFeesController);// add fees to late rentals
+router.delete("/:id", rentalController.deleteRental);//delete rental by admin
+router.put("/:id", rentalController.updateRental);//update rental info
+router.post("/decision", rentalController.acceptOrDenyRental);//accept or deny renting request by owner
 
 module.exports = router;
