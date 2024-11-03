06e5adfb0755157104a522cc157c8cf49992bf59
marah3 <115647586+marah3@users.noreply.github.com>
2024-11-03 17:58:26 +0200

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -4,7 +4,7 @@ const express = require("express");
 const paymentController = require("../controllers/paymentController");
 const router = express.Router();
 
-// Define the route for creating a payment intent
+
 router.post("/payment-intent", paymentController.createPaymentIntent);
 
 module.exports = router;
