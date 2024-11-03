a3331250ccef360e2a43e9d6f200cee6290bfd84
Raghad.Matar <164626769+RaghadMM@users.noreply.github.com>
2024-11-03 18:16:53 +0200

    notification model

--- models/notificationModel.js
+++ models/notificationModel.js
@@ -1,9 +1,9 @@
 const Notification = {
-  notification_id: null,
-  user_id: null,
-  message: null,
-  status: null,
-  created_at: new Date(),
+    notification_id: null,
+    user_id: null,
+    message: null,
+    status: null,
+    created_at: new Date()
 };
 
 // Export the Rental model
