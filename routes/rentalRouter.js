ce3f4ac126372585ff7801cdb86368b0afe472e7
Raghad.Matar <164626769+RaghadMM@users.noreply.github.com>
2024-11-04 00:53:58 +0200

    update rentalRouter/final

--- routes/rentalRouter.js
+++ routes/rentalRouter.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const rentalController = require('../controllers/rentalController'); // Adjust the path as necessary
+const rentalController = require("../controllers/rentalController"); // Adjust the path as necessary
 
-// Register a new rental
-router.post('/Rentals', rentalController.registerRental);
-router.get('/AllRentals', rentalController.getAllRentals);
-router.post('/Fees', rentalController.updateLateFeesController);
-router.delete('/Rentals/:id', rentalController.deleteRental);
-router.get('/Rentals', rentalController.getRentals);
-router.put('/Rentals/:id', rentalController.updateRental);
+
+router.post("/", rentalController.registerRental);//register a new rental
+router.get("/AllRentals", rentalController.getAllRentals);//get all rentals for admin
+router.get("/", rentalController.getRentals);// search on items by specific field
+router.get("/LateRentals", rentalController.getLateRentals);//get late unretrieved rentals
+router.put("/Fees", rentalController.updateLateFeesController);// add fees to late rentals
+router.delete("/:id", rentalController.deleteRental);//delete rental by admin
+router.put("/:id", rentalController.updateRental);//update rental info
+router.post("/decision", rentalController.acceptOrDenyRental);//accept or deny renting request by owner
+
 module.exports = router;
