cccfa342d67230123c5219cdf7e4f68f16de6edc
Raghad.Matar <164626769+RaghadMM@users.noreply.github.com>
2024-11-03 18:16:07 +0200

    Add files via upload

--- models/rentalModel.js
+++ models/rentalModel.js
@@ -1,14 +1,15 @@
+
 const Rental = {
-  rental_id: null,
-  item_id: null,
-  renter_id: null,
-  owner_id: null,
-  start_date: null,
-  end_date: null,
-  total_price: null,
-  late_fee: null,
-  status: null,
-  created_at: new Date(),
+    rental_id: null,
+    item_id: null,
+    renter_id: null,
+    owner_id: null,
+    start_date: null,
+    end_date: null,
+    total_price: null,
+    late_fee: null,
+    status: null,
+    created_at: new Date()
 };
 
 // Export the Rental model
