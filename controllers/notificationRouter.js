3036486a5144c58508c3c1eccc3c61cc80e8b721
Raghad.Matar <164626769+RaghadMM@users.noreply.github.com>
2024-11-04 00:54:56 +0200

    Delete controllers/notificationRouter.js

--- controllers/notificationRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notificationController = require('../controllers/notificationController'); // Adjust the path as necessary
-router.post('/Notifications', notificationController.createNotification);
-router.get('/Notifications/:id', notificationController.getUserNotifications);
-router.put('/markAsRead/:id', notificationController.markAsRead);
-router.delete('/Notifications/:id', notificationController.deleteNotification);
-module.exports = router;
