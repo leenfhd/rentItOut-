c5049a6742e2782c845a3e92c485e65ad798fbb7
Raghad.Matar <164626769+RaghadMM@users.noreply.github.com>
2024-11-03 18:29:26 +0200

    rentalRouter

--- routes/rentalRouter.js
+++ routes/rentalRouter.js
@@ -1,13 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const rentalController = require("../controllers/rentalController"); // Adjust the path as necessary
+const rentalController = require('../controllers/rentalController'); // Adjust the path as necessary
 
 // Register a new rental
-router.post("/Rentals", rentalController.registerRental);
-router.get("/AllRentals", rentalController.getAllRentals);
-router.post("/Fees", rentalController.updateLateFeesController);
-router.delete("/Rentals/:id", rentalController.deleteRental);
-router.get("/Rentals", rentalController.getRentals);
-router.put("/Rentals/:id", rentalController.updateRental);
-router.post("/checkPendings", rentalController.acceptOrDenyRental);
+router.post('/Rentals', rentalController.registerRental);
+router.get('/AllRentals', rentalController.getAllRentals);
+router.post('/Fees', rentalController.updateLateFeesController);
+router.delete('/Rentals/:id', rentalController.deleteRental);
+router.get('/Rentals', rentalController.getRentals);
+router.put('/Rentals/:id', rentalController.updateRental);
 module.exports = router;
