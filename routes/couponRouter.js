7978c568af1cf5c1e3294df756455927fa384534
marah3 <115647586+marah3@users.noreply.github.com>
2024-11-03 17:57:23 +0200

    Update couponRouter.js

--- routes/couponRouter.js
+++ routes/couponRouter.js
@@ -3,19 +3,19 @@ const couponController = require("../controllers/couponController");
 
 const router = express.Router();
 
-// Create a coupon
+
 router.post("/", couponController.createCoupon);
 
-// Get all coupons
+
 router.get("/", couponController.getAllCoupons);
 
-// Get a specific coupon by ID
+
 router.get("/:id", couponController.getCouponById);
 
-// Update a coupon by ID
+
 router.patch("/:id", couponController.updateCoupon);
 
-// Delete a coupon by ID
+
 router.delete("/:id", couponController.deleteCoupon);
 
 module.exports = router;
