9f88403c0fcc19a2a9c4b1c4d76587e30511d8ea
Raghad.Matar <164626769+RaghadMM@users.noreply.github.com>
2024-11-04 00:53:34 +0200

    update notificationRouter/final

--- routes/notificationRouter.js
+++ routes/notificationRouter.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const notificationController = require('../controllers/notificationController'); // Adjust the path as necessary
-router.post('/Notifications', notificationController.createNotification);
-router.get('/Notifications/:id', notificationController.getUserNotifications);
-router.put('/markAsRead/:id', notificationController.markAsRead);
-router.delete('/Notifications/:id', notificationController.deleteNotification);
+const notificationController = require("../controllers/notificationController"); 
+router.post("/", notificationController.createNotification);//create specific notification
+router.get("/", notificationController.getUserNotifications);//get user notifications
+router.put("/markAsRead/:id", notificationController.markAsRead);//mark notification as read
+router.delete("/:id", notificationController.deleteNotification);//delete notification
 module.exports = router;
